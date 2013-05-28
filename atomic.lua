ebd15583e123c7441ad14e9f04205941cd4fae00
kirilligum <bribeme@gmail.com>
2013-05-28 13:49:08 -0700

    Create atomic.lua

---
+++ atomic.lua
@@ -0,0 +1,34 @@
+----------------------------------------
+----------------------------------------
+------ Atomic units (from http://en.wikipedia.org/wiki/Atomic_units )
+----------------------------------------
+---- Fundamental atomic units in SI
+-- mass
+electron_rest_mass =   9.1093829140e-31; -- kg;
+-- charge
+elementary_charge =  1.60217656535e-19; -- C;
+-- angular momentum
+reduced_Plancks_constant =  1.05457172647e-34; --  J*s;
+-- electric constant
+Coulomb_force_constant =   8.9875517873681e9; -- kg*m^3*s^(-2)*C-^(-2);
+----------------------------------------
+---- Derived atomic units in SI
+-- length
+bohr_radius =    5.291772085936e-11; -- m  ;
+-- energy
+hartree_energy =     4.3597441775e-18; -- J ;
+-- time
+atomic_time =    2.41888432650516-e17; -- s;
+-- velocity
+atomic_velocity = 2.187691263373e6; -- m*s^(-1);
+-- force
+atomic_force = 8.238722514e-8; --  N;
+-- temperature
+atomic_temperature = 3.157746455e05; -- K  ;
+-- pressure
+atomic_pressure = 2.942191219e13; -- Pa  ;
+-- electric field
+atomic_electric_field = 5.1422065211e11; --  V*m^(-1);
+-- electric dipole moment
+atomic_electric_dipole_moment = 8.4783532619e-30; --  C*m;
+
